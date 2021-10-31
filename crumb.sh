1ea363edd6876e3867d60d17e7f84b35866e5429
Paul <a44387521@gmail.com>
2021-10-31 10:58:58 +0800

    Trigger jobs from bash script (with parameters)

--- crumb.sh
+++ crumb.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
 crumb=$(curl -u "jenkins:1234" -s 'http://jenkins.local:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
-curl -u "jenkins:1234" -H "$crumb" -X POST http://jenkins.localhost:8080/job/ENV/build?delay=0sec
+#curl -u "jenkins:1234" -H "$crumb" -X POST http://jenkins.localhost:8080/job/ENV/build?delay=0sec
+curl -u "jenkins:1234" -H "$crumb" -X POST  http://jenkins.localhost:8080/job/ansible-users-db/buildWithParameters?AGE=25
